6ae656f1914a4c8fb118ce1ade1b62cb3c93a8dd
m4sbay <maulanabayu026@gmail.com>
2024-06-20 07:39:29 +0700

    //register

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -43,7 +43,7 @@ new #[Layout('layouts.guest')] class extends Component
         <div class="col-lg-7">
             <div class="p-5">
                 <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Create an New Account!</h1>
+                    <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                 </div>
                 <form class="user">
                     <div class="form-group row">
