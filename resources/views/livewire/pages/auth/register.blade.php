2e15c757b63b1c0605c13e6e0f1922c860feabfa
Azwary <azwrysf23@gmail.com>
2024-06-22 11:51:16 +0700

    reister  fix

--- resources/views/livewire/pages/auth/register.blade.php
+++ resources/views/livewire/pages/auth/register.blade.php
@@ -8,8 +8,7 @@ use Illuminate\Validation\Rules;
 use Livewire\Attributes\Layout;
 use Livewire\Volt\Component;
 
-new #[Layout('layouts.guest')] class extends Component
-{
+new #[Layout('layouts.guest')] class extends Component {
     public string $name = '';
     public string $email = '';
     public string $password = '';
@@ -22,22 +21,20 @@ new #[Layout('layouts.guest')] class extends Component
     {
         $validated = $this->validate([
             'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:' . User::class],
             'password' => ['required', 'string', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $validated['password'] = Hash::make($validated['password']);
 
-        event(new Registered($user = User::create($validated)));
+        event(new Registered(($user = User::create($validated))));
 
         Auth::login($user);
 
         $this->redirect(route('dashboard', absolute: false), navigate: true);
     }
 }; ?>
-
 <div class="">
-
     <div class="row">
         <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
         <div class="col-lg-7">
@@ -45,51 +42,80 @@ new #[Layout('layouts.guest')] class extends Component
                 <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
                 </div>
-                <form class="user">
-                    <div class="form-group row">
-                        <div class="col-sm-6 mb-3 mb-sm-0">
-                            <input type="text" class="form-control form-control-user" id="exampleFirstName"
-                                placeholder="First Name">
+                <form wire:submit="register">
+                    <div class="form-group">
+                        <!-- Name -->
+                        {{-- <div class="col-sm-6 mb-3 mb-sm-0"> --}}
+                        <div class="">
+                            <x-input-label for="name" :value="__('Name')" />
+                            <x-text-input wire:model="name" id="name" class="block mt-1 w-full" type="text"
+                                name="name" required autofocus autocomplete="name" placeholder="Name" />
+                            <x-input-error :messages="$errors->get('name')" class="mt-2" />
                         </div>
-                        <div class="col-sm-6">
+                        {{-- <div class="col-sm-6">
                             <input type="text" class="form-control form-control-user" id="exampleLastName"
                                 placeholder="Last Name">
-                        </div>
+                        </div> --}}
                     </div>
+                    <!-- Email Address -->
                     <div class="form-group">
-                        <input type="email" class="form-control form-control-user" id="exampleInputEmail"
-                            placeholder="Email Address">
+                        <div class="mt-4">
+                            <x-input-label for="email" :value="__('Email')" />
+                            <x-text-input wire:model="email" id="email" class="block mt-1 w-full" type="email"
+                                name="email" required autocomplete="username" placeholder="Email Address" />
+                            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                        </div>
                     </div>
                     <div class="form-group row">
+                        <!-- Password -->
                         <div class="col-sm-6 mb-3 mb-sm-0">
-                            <input type="password" class="form-control form-control-user"
-                                id="exampleInputPassword" placeholder="Password">
+                            <x-input-label for="password" :value="__('Password')" />
+
+                            <x-text-input wire:model="password" id="password" class="block mt-1 w-full" type="password"
+                                name="password" required autocomplete="new-password" placeholder="Password" />
+
+                            <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         </div>
+                        <!-- Confirm Password -->
                         <div class="col-sm-6">
-                            <input type="password" class="form-control form-control-user"
-                                id="exampleRepeatPassword" placeholder="Repeat Password">
+                            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                            <x-text-input wire:model="password_confirmation" id="password_confirmation"
+                                class="block mt-1 w-full" type="password" name="password_confirmation" required
+                                autocomplete="new-password" placeholder="Confirm Password" />
+
+                            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                         </div>
                     </div>
-                    <a href="login.html" class="btn btn-primary btn-user btn-block">
+                    <div class="flex items-center justify-end mt-4">
+                        {{-- <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}" wire:navigate>
+                            {{ __('Already registered?') }}
+                        </a> --}}
+
+                        {{-- <x-primary-button class="ms-4"> --}}
+                        <x-primary-button class="btn btn-primary btn-user btn-block">
+                            {{ __('Register') }}
+                        </x-primary-button>
+                    </div>
+                    {{-- <a href="login.html" class="btn btn-primary btn-user btn-block">
                         Register Account
-                    </a>
-                    <hr>
+                    </a> --}}
+                    {{-- <hr>
                     <a href="index.html" class="btn btn-google btn-user btn-block">
                         <i class="fab fa-google fa-fw"></i> Register with Google
                     </a>
                     <a href="index.html" class="btn btn-facebook btn-user btn-block">
                         <i class="fab fa-facebook-f fa-fw"></i> Register with Facebook
-                    </a>
+                    </a> --}}
                 </form>
                 <hr>
                 <div class="text-center">
-                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                    <a class="small"  href="{{ route('password.request') }}">Forgot Password?</a>
                 </div>
                 <div class="text-center">
-                    <a class="small" href="login.html">Already have an account? Login!</a>
+                    <a class="small" href="{{ route('login') }}">Already have an account? Login!</a>
                 </div>
             </div>
         </div>
     </div>
-
 </div>
