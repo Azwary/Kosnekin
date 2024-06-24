3d8371dd0f5aa9f43a77d3629913761f4d0c1cc1
Azwary <azwrysf23@gmail.com>
2024-06-24 08:34:36 +0700

    terbaru

---
+++ app/Livewire/Content/Kriteria.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Content;
+
+use Livewire\Component;
+
+class Kriteria extends Component
+{
+    public function render()
+    {
+        return view('livewire.content.kriteria');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Livewire\Content\Kriteria;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
-
+Route::get('kriteria',Kriteria::class);
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
     ->name('dashboard');
