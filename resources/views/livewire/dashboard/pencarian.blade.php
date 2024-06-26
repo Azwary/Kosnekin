8a2ee4cb20e885b565204ba80a9f825419d452b4
Azwary <azwrysf23@gmail.com>
2024-06-26 19:34:37 +0700

    erlan(progrest)

--- resources/views/livewire/dashboard/pencarian.blade.php
+++ resources/views/livewire/dashboard/pencarian.blade.php
@@ -1,3 +1,164 @@
-<div>
-    {{-- Do your work, then step back. --}}
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <div class="d-sm-flex align-items-center justify-content-between mb-4">
+        <h1 class="h3 mb-0 text-gray-800">Rekomendasi Kos</h1>
+    </div>
+
+    <!-- Content Row -->
+    <div class="row">
+        <!-- Ranking Cards -->
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="ribbon"><span>Rekomendasi 1</span></div>
+                <img src="{{asset('img/pict1.png')}}" class="card-img-top img-fluid" alt="...">
+                <div class="card-divider"></div> <!-- Divider line -->
+                <div class="card-body">
+                    <h5 class="card-title">Runi House</h5>
+                    <p class="card-text">Alamat: Jl. bla bla no 18</p>
+                    <p class="card-text">Biaya (bulan): Rp.700.000</p>
+                    <p class="card-text">Keamanan: CCTV</p>
+                    <p class="card-text">Ukuran ruangan: 4 x 5</p>
+                    <p class="card-text">Fasilitas: Lengkap</p>
+                    <p class="card-text">Batas jam malam: 24 jam</p>
+                    <p class="card-text">Jenis listrik: Token</p>
+                    <p class="card-text">Kebersihan: Bersih</p>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="ribbon"><span>Rekomendasi 2</span></div>
+                <img src="{{asset('img/pict2.jpg')}}" class="card-img-top img-fluid" alt="...">
+                <div class="card-divider"></div> <!-- Divider line -->
+                <div class="card-body">
+                    <h5 class="card-title">Runi House</h5>
+                    <p class="card-text">Alamat: Jl. bla bla no 18</p>
+                    <p class="card-text">Biaya (bulan): Rp.700.000</p>
+                    <p class="card-text">Keamanan: CCTV</p>
+                    <p class="card-text">Ukuran ruangan: 4 x 5</p>
+                    <p class="card-text">Fasilitas: Lengkap</p>
+                    <p class="card-text">Batas jam malam: 24 jam</p>
+                    <p class="card-text">Jenis listrik: Token</p>
+                    <p class="card-text">Kebersihan: Bersih</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="ribbon"><span>Rekomendasi 3</span></div>
+                <img src="{{asset('img/pict2.jpg')}}" class="card-img-top img-fluid" alt="...">
+                <div class="card-divider"></div> <!-- Divider line -->
+                <div class="card-body">
+                    <h5 class="card-title">Runi House</h5>
+                    <p class="card-text">Alamat: Jl. bla bla no 18</p>
+                    <p class="card-text">Biaya (bulan): Rp.700.000</p>
+                    <p class="card-text">Keamanan: CCTV</p>
+                    <p class="card-text">Ukuran ruangan: 4 x 5</p>
+                    <p class="card-text">Fasilitas: Lengkap</p>
+                    <p class="card-text">Batas jam malam: 24 jam</p>
+                    <p class="card-text">Jenis listrik: Token</p>
+                    <p class="card-text">Kebersihan: Bersih</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="ribbon"><span>Rekomendasi 4</span></div>
+                <img src="{{asset('img/pict2.jpg')}}" class="card-img-top img-fluid" alt="...">
+                <div class="card-divider"></div> <!-- Divider line -->
+                <div class="card-body">
+                    <h5 class="card-title">Runi House</h5>
+                    <p class="card-text">Alamat: Jl. bla bla no 18</p>
+                    <p class="card-text">Biaya (bulan): Rp.700.000</p>
+                    <p class="card-text">Keamanan: CCTV</p>
+                    <p class="card-text">Ukuran ruangan: 4 x 5</p>
+                    <p class="card-text">Fasilitas: Lengkap</p>
+                    <p class="card-text">Batas jam malam: 24 jam</p>
+                    <p class="card-text">Jenis listrik: Token</p>
+                    <p class="card-text">Kebersihan: Bersih</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="ribbon"><span>Rekomendasi 5</span></div>
+                <img src="{{asset('img/pict2.jpg')}}" class="card-img-top img-fluid" alt="...">
+                <div class="card-divider"></div> <!-- Divider line -->
+                <div class="card-body">
+                    <h5 class="card-title">Runi House</h5>
+                    <p class="card-text">Alamat: Jl. bla bla no 18</p>
+                    <p class="card-text">Biaya (bulan): Rp.700.000</p>
+                    <p class="card-text">Keamanan: CCTV</p>
+                    <p class="card-text">Ukuran ruangan: 4 x 5</p>
+                    <p class="card-text">Fasilitas: Lengkap</p>
+                    <p class="card-text">Batas jam malam: 24 jam</p>
+                    <p class="card-text">Jenis listrik: Token</p>
+                    <p class="card-text">Kebersihan: Bersih</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="ribbon"><span>Rekomendasi 6</span></div>
+                <img src="{{asset('img/pict2.jpg')}}" class="card-img-top img-fluid" alt="...">
+                <div class="card-divider"></div> <!-- Divider line -->
+                <div class="card-body">
+                    <h5 class="card-title">Runi House</h5>
+                    <p class="card-text">Alamat: Jl. bla bla no 18</p>
+                    <p class="card-text">Biaya (bulan): Rp.700.000</p>
+                    <p class="card-text">Keamanan: CCTV</p>
+                    <p class="card-text">Ukuran ruangan: 4 x 5</p>
+                    <p class="card-text">Fasilitas: Lengkap</p>
+                    <p class="card-text">Batas jam malam: 24 jam</p>
+                    <p class="card-text">Jenis listrik: Token</p>
+                    <p class="card-text">Kebersihan: Bersih</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="ribbon"><span>Rekomendasi 7</span></div>
+                <img src="{{asset('img/pict2.jpg')}}" class="card-img-top img-fluid" alt="...">
+                <div class="card-divider"></div> <!-- Divider line -->
+                <div class="card-body">
+                    <h5 class="card-title">Runi House</h5>
+                    <p class="card-text">Alamat: Jl. bla bla no 18</p>
+                    <p class="card-text">Biaya (bulan): Rp.700.000</p>
+                    <p class="card-text">Keamanan: CCTV</p>
+                    <p class="card-text">Ukuran ruangan: 4 x 5</p>
+                    <p class="card-text">Fasilitas: Lengkap</p>
+                    <p class="card-text">Batas jam malam: 24 jam</p>
+                    <p class="card-text">Jenis listrik: Token</p>
+                    <p class="card-text">Kebersihan: Bersih</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card" style="width: 18rem;">
+                <div class="ribbon"><span>Rekomendasi 8</span></div>
+                <img src="{{asset('img/pict2.jpg')}}" class="card-img-top img-fluid" alt="...">
+                <div class="card-divider"></div> <!-- Divider line -->
+                <div class="card-body">
+                    <h5 class="card-title">Runi House</h5>
+                    <p class="card-text">Alamat: Jl. bla bla no 18</p>
+                    <p class="card-text">Biaya (bulan): Rp.700.000</p>
+                    <p class="card-text">Keamanan: CCTV</p>
+                    <p class="card-text">Ukuran ruangan: 4 x 5</p>
+                    <p class="card-text">Fasilitas: Lengkap</p>
+                    <p class="card-text">Batas jam malam: 24 jam</p>
+                    <p class="card-text">Jenis listrik: Token</p>
+                    <p class="card-text">Kebersihan: Bersih</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End of Content Row -->
+
 </div>
